a064564266e6713594f9aa016dff13ac438bfb46
AlxDmk <87035808+AlxDmk@users.noreply.github.com>
2022-10-29 22:55:00 +0300

    Добавьте файлы проекта.

---
+++ PumpService/IPumpService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PumpService
+{
+   
+    [ServiceContract(Namespace = "https://Microsoft.ServiceModels.Samples", SessionMode = SessionMode.Required, CallbackContract = typeof(IPumpServiceCallback))]
+    public interface IPumpService
+    {
+        [OperationContract]
+        void RunScript();
+
+        [OperationContract]
+        void UpdateAndCompileScript(string fileName);       
+    }   
+}
---
+++ PumpService/ScriptService.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PumpService
+{
+    public class ScriptService : IScriptService
+    {
+        private CompilerResults result = null;
+        private readonly IStatisticsService _statisticsService;
+        private readonly ISettingsService _settingsService;
+        private readonly IPumpServiceCallback _pumpServiceCallback;
+
+        public ScriptService(IStatisticsService statisticsService, IPumpServiceCallback pumpServiceCallback, ISettingsService settingsService)
+        {
+            _statisticsService = statisticsService;
+            _pumpServiceCallback = pumpServiceCallback;
+            _settingsService = settingsService;
+        }
+        public bool Compile()
+        {
+            try
+            {
+                CompilerParameters compilerParameters = new CompilerParameters();
+                compilerParameters.GenerateInMemory = true;
+                compilerParameters.ReferencedAssemblies.Add("System.dll");
+                compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
+                compilerParameters.ReferencedAssemblies.Add("System.Data.dll");
+                compilerParameters.ReferencedAssemblies.Add("Microsoft.CSharp.dll");
+                compilerParameters.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+
+                FileStream fileStream = new FileStream(_settingsService.FileName, FileMode.Open);
+                byte[] buffer;
+                try
+                {
+                    int length = (int)fileStream.Length;
+                    buffer = new byte[length];
+                    int count;
+                    int sum = 0;
+                    while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+                        sum += count;
+                }
+                finally
+                {
+                    fileStream.Close();
+                }
+
+                CSharpCodeProvider provider = new CSharpCodeProvider();
+                result = provider.CompileAssemblyFromSource(compilerParameters, System.Text.Encoding.UTF8.GetString(buffer));
+                if (result.Errors != null && result.Errors.Count > 0)
+                {
+                    string compileErrors = string.Empty;
+                    for (int i = 0; i < result.Errors.Count; i++)
+                    {
+                        if (compileErrors != string.Empty)
+                        {
+                            compileErrors += "\n";
+                        }
+                        compileErrors += result.Errors[i];
+                    }
+
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+         
+        }
+
+        public void Run(int count)
+        {
+            if(result == null || (result != null && result.Errors != null && result.Errors.Count > 0))
+            {
+                if(Compile() == false)
+                {
+                    return;
+                }
+            }
+            Type t = result.CompiledAssembly.GetType("Sample.SampleScript");
+            if (t == null)
+            {
+                return;
+            }
+            MethodInfo entryMethodPoint = t.GetMethod("EntryPoint");
+            if (entryMethodPoint == null)
+            {
+                return;
+            }
+
+            Task.Run(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if ((bool)entryMethodPoint.Invoke(Activator.CreateInstance(t), null))
+                    {
+                        _statisticsService.SuccessTacts++;
+                    }
+                    else
+                    {
+                        _statisticsService.ErrorTacts++;
+                    }
+                    _statisticsService.AllTacts++;
+                    _pumpServiceCallback.UpdateStatistics((StatisticsService)_statisticsService);
+                    Thread.Sleep(1000);
+                };
+
+            });            
+
+        }
+    }
+}
